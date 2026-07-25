1090ef25e58d24a19014f82a32b32bf1d9cb29ca
saira <sairaazram965@gmail.com>
2026-07-25 20:25:47 +0500

    Add Django project configuration files Include settings, urls, wsgi, asgi and views

--- urls.py
+++ urls.py
@@ -1,19 +1,29 @@
-from django.urls import path
-from . import views
+"""
+URL configuration for ecommerce project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/6.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('product/<int:product_id>/', views.product_detail, name='product_detail'),
-    path('cart/', views.cart_detail, name='cart_detail'),
-    path('cart/add/<int:product_id>/', views.add_to_cart, name='add_to_cart'),
-    path('cart/remove/<int:product_id>/', views.remove_from_cart, name='remove_from_cart'),
-    path('cart/update/<int:product_id>/', views.update_cart, name='update_cart'),
-    path('checkout/', views.checkout, name='checkout'),
-    path('order/success/<int:order_id>/', views.order_success, name='order_success'),
-    path('signup/', views.signup_view, name='signup'),
-    path('login/', views.login_view, name='login'),
-    path('logout/', views.logout_view, name='logout'),
-    path('my-orders/', views.my_orders, name='my_orders'),
-    path('hot-offers/', views.hot_offers, name='hot_offers'),   # 👈 NAYI LINE
-    path('blog/', views.blog, name='blog'),                     # 👈 NAYI LINE
+    path('admin/', admin.site.urls),
+    path('', include('store.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
